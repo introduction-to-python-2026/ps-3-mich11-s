14e7bee41240a28f100369ca4aa7f6ac4a1e144d
mich11-s <michbc2812@gmail.com>
2025-11-10 18:29:42 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,8 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  list_of_numbers = [1] * n_terms
+  approximation_factor = 0
+  for i in range(n_terms):
+    nominator = (-1) ** i
+    denominator = 2 * i + 1
+    approximation_factor = approximation_factor + (nominator / denominator)
+  print(4 * approximation_factor)
